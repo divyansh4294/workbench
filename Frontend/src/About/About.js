d54879dd0dc0f2930476a8e6a743cc1ce3f5d7f7
divyansh gupta <45636435+divyansh4294@users.noreply.github.com>
2021-06-11 18:35:06 +0530

    Add files via upload

---
+++ Frontend/src/About/About.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+    render() {
+        return (
+            <div style={{justifyContent: 'center', padding: 30 }}>
+                <h1>Our Department</h1>
+                <h4>We believe that making machine learning accessible represents our greatest opportunity to empower the lives of everyday business users.
+                That's why we have built a tool that enables non-coders to rapidly run predictions on their historical data, with just a few clicks.
+                Allowing them to make better business decisions, fast.
+                We are A&SI, a Data Science Department at JPL based in Navi Mumbai, Maharastra, India.
+                </h4>
+            </div>
+        );
+    }
+}
+
+export default About;
